30d267506d5959ca481db92e0ec3ed1c320ff02f
gabrielfontineli <gabriel0fontineli@gmail.com>
2024-04-14 15:02:30 -0300

    feat: refactor edit method for aluno

--- src/main/java/com/imd/university/controllers/AlunoController.java
+++ src/main/java/com/imd/university/controllers/AlunoController.java
@@ -4,18 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imd.university.repository.AlunoRepository;
+
+import jakarta.validation.Valid;
+
 import com.imd.university.dto.AlunoDTO;
 import com.imd.university.model.Aluno;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -27,7 +33,7 @@ public class AlunoController {
     private AlunoRepository alunoRepository;
 
     @PostMapping("/create")
-    public ResponseEntity<Aluno> createAluno(@RequestBody AlunoDTO entity) {
+    public ResponseEntity<Aluno> createAluno(@Valid @RequestBody AlunoDTO entity) {
         Aluno aluno = new Aluno(entity);
         assert aluno.getDataNascimento() != null: "Data de nascimento não pode ser nula";
 
@@ -42,13 +48,24 @@ public class AlunoController {
     public ResponseEntity<Aluno> getAluno(@RequestParam Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(alunoRepository.findById(id).get());
     }
-    @PostMapping("/update")
-    public ResponseEntity<Aluno> updateAluno(@RequestBody AlunoDTO entity) {
-        Aluno aluno = new Aluno(entity);
-        assert aluno.getDataNascimento() != null: "Data de nascimento não pode ser nula";
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> updateAluno(@RequestBody AlunoDTO entity, @PathVariable Long id) {
+        Optional<Aluno> alunoOptional = alunoRepository.findById(id);
+        if (alunoOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        Aluno aluno = alunoOptional.get();
+        Optional.ofNullable(entity.nome()).ifPresent(aluno::setNome);
+        if (entity.matricula() != 0) {
+            aluno.setMatricula(entity.matricula());
+        }
+        Optional.ofNullable(entity.cpf()).ifPresent(aluno::setCpf);
+        Optional.ofNullable(entity.genero()).ifPresent(aluno::setGenero);
+        Optional.ofNullable(entity.curso()).ifPresent(aluno::setCurso);
+        Optional.ofNullable(entity.dataNascimento()).ifPresent(aluno::setDataNascimento);
 
-        Aluno savedAluno = alunoRepository.save(aluno);
-        return ResponseEntity.status(HttpStatus.OK).body(savedAluno);
+        Aluno updatedAluno = alunoRepository.save(aluno);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedAluno);
     }
     @PostMapping("/delete")
     public ResponseEntity<String> deleteAluno(@RequestParam Long id) {
--- src/main/java/com/imd/university/repository/AlunoRepository.java
+++ src/main/java/com/imd/university/repository/AlunoRepository.java
@@ -2,9 +2,7 @@ package com.imd.university.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.imd.university.model.Aluno; // Add missing import statement
-import java.util.List;
-import java.util.Optional;
+import com.imd.university.model.Aluno; 
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {

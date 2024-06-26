badf266e06ed7f0a4c579476c2a6b2efbafb41a6
gabrielfontineli <gabriel0fontineli@gmail.com>
2024-06-26 08:31:45 -0300

    adjust authentication manager

--- src/main/java/com/imd/university/security/SecurityConfiguration.java
+++ src/main/java/com/imd/university/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,30 +18,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
-        return http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login" ).permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/professor").hasRole("ADMIN")
-                        
-
-                        .anyRequest().authenticated()
-                )
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .requestMatchers(HttpMethod.GET, "/alunos", "/turma", "/professores", "/usuarios").hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/alunos", "/turma", "/professores", "/usuarios").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/alunos", "/turma", "/professores", "/usuarios").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/alunos", "/turma", "/professores", "/usuarios").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> {});
+        return http.build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return  authenticationConfiguration.getAuthenticationManager();
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
